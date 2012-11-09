d37d15ae302da33942c4a55d8b5e275ef1b6502a
sandervanloock <lieserulez@hotmail.com>
2012-11-09 13:52:14 +0100

    initial sencha commit

--- app/view/Blog.js
+++
@@ -1,9 +0,0 @@
-Ext.define("Sencha.view.Blog",{
-	extend: 'Ext.List',
-	xtype: 'blogpage',
-	config:{
-		title: 'Blog',
-		iconCls: 'star',
-		itemTpl: '{title}'
-	}
-});
--- app/view/Home.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Sencha.view.Home',{
-	extend: 'Ext.Panel',
-	xtype: 'homepanel',
-	config: {
-		title : 'Home',
-		iconCls : 'home',
-		html : 'Home'
-	}
-});

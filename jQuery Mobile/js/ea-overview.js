c50e6e4cbe5a69dd4d2405d9136b1b0576df00a4
Tim Ameye <Tim@10-92-99-236.kotnet.org>
2012-11-20 10:52:42 +0100

    overview now shows load screen

--- jQuery Mobile/js/ea-overview.js
+++ jQuery Mobile/js/ea-overview.js
@@ -1,40 +1,36 @@
-$(document).on("pagebeforeshow", "#overview", function () {
-    if (!EA.isLoggedIn()) {
-        EA.redirectNotLoggedIn();
-    } else {
-        $.ajax({
-            type:"POST",
-            dataType:"xml",
-            url:"http://kulcapexpenseapp.appspot.com/resources/expenseService/getExpenseForms",
-            data:{
-                'token':EA.token
-            },
-            beforeSend:function () {
-                $.mobile.loading("show");
-            },
-            success:function (xml) {
-                $("#overview-list").empty();
-                $(xml).find("expenseForm").each(function () {
-                    // put $(this) in avariable for performance reasons
-                    var $this = $(this);
+$(document).on("pageshow", "#overview", function () {
+    $.ajax({
+        type:"POST",
+        dataType:"xml",
+        url:"http://kulcapexpenseapp.appspot.com/resources/expenseService/getExpenseForms",
+        data:{
+            'token':EA.token
+        },
+        beforeSend:function () {
+            $.mobile.loading("show");
+        },
+        success:function (xml) {
+            $("#overview-list").empty();
+            $(xml).find("expenseForm").each(function () {
+                // put $(this) in avariable for performance reasons
+                var $this = $(this);
 
-                    // > finds the direct descendant in tree
-                    var date = new Date($this.find(">date").text());
-                    var id = $this.find(">id").text();
+                // > finds the direct descendant in tree
+                var date = new Date($this.find(">date").text());
+                var id = $this.find(">id").text();
 
-                    var dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
-                    $("#overview-list").append("<li><a id=\"overview-show-pdf-" + id + "\"><h1>" + dateString + "</h1><p>Status</p></a></li>");
-                });
-                $("#overview-list").listview("refresh");
-            },
-            error:function (xhr, textStatus, errorThrown) {
-                EA.showErrorDialog("Backend error: " + xhr.status, errorThrown);
-            },
-            complete:function () {
-                $.mobile.loading("hide");
-            }
-        });
-    }
+                var dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+                $("#overview-list").append("<li><a id=\"overview-show-pdf-" + id + "\"><h1>" + dateString + "</h1><p>Status</p></a></li>");
+            });
+            $("#overview-list").listview("refresh");
+        },
+        error:function (xhr, textStatus, errorThrown) {
+            EA.showErrorDialog("Backend error: " + xhr.status, errorThrown);
+        },
+        complete:function () {
+            $.mobile.loading("hide");
+        }
+    });
 });
 
 $(document).on("tap", "[id^=overview-show-pdf]", function () {

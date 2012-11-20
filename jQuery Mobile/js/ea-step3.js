fc3991f2ca4b0f48284a49150c76ca6ff58adb24
Tim Ameye <Tim@10-92-99-236.kotnet.org>
2012-11-20 15:03:27 +0100

    validation when adding an expense

---
+++ jQuery Mobile/js/ea-step3.js
@@ -0,0 +1,26 @@
+$(document).on("pageinit", "#step3", function () {
+    $("#step3-form").validate({
+        rules:{
+            "step3-date":{
+                required:true
+            },
+            "step3-project-code":{
+                required:true
+            },
+            "step3-amount":{
+                required:true,
+                min:0 // a negative amount is not valid
+            },
+            "step3-remarks":{
+
+            }
+        },
+        focusInvalid:false,
+        submitHandler:function (form) {
+            $.mobile.changePage("#step4");
+        },
+        invalidHandler:function (form, validator) {
+            EA.showErrorDialog("Validation error", "Some of the fields were not filled in correctly. Please correct the indicated fields.");
+        }
+    });
+});

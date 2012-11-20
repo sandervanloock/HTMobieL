ca88f1d6bc929c2c938e92ede4ab10c27a160111
Tim Ameye <Tim@10-92-99-236.kotnet.org>
2012-11-20 10:47:15 +0100

    open pdf from overview

--- jQuery Mobile/js/ea-overview.js
+++ jQuery Mobile/js/ea-overview.js
@@ -15,11 +15,15 @@ $(document).on("pagebeforeshow", "#overview", function () {
             success:function (xml) {
                 $("#overview-list").empty();
                 $(xml).find("expenseForm").each(function () {
+                    // put $(this) in avariable for performance reasons
                     var $this = $(this);
-                    // find direct date descendant
+
+                    // > finds the direct descendant in tree
                     var date = new Date($this.find(">date").text());
+                    var id = $this.find(">id").text();
+
                     var dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
-                    $("#overview-list").append("<li><a><h1>" + dateString + "</h1><p>Status</p></a></li>");
+                    $("#overview-list").append("<li><a id=\"overview-show-pdf-" + id + "\"><h1>" + dateString + "</h1><p>Status</p></a></li>");
                 });
                 $("#overview-list").listview("refresh");
             },
@@ -32,3 +36,28 @@ $(document).on("pagebeforeshow", "#overview", function () {
         });
     }
 });
+
+$(document).on("tap", "[id^=overview-show-pdf]", function () {
+    var expenseFormId = $(this).attr("id").replace("overview-show-pdf-", "");
+    $.ajax({
+        type:"POST",
+        //dataType:"xml",
+        url:"http://kulcapexpenseapp.appspot.com/resources/expenseService/getExpenseFormPDF",
+        data:{
+            'token':EA.token,
+            'expenseFormId':expenseFormId
+        },
+        beforeSend:function () {
+            $.mobile.loading("show");
+        },
+        success:function (pdf) {
+            console.log(pdf);
+        },
+        error:function (xhr, textStatus, errorThrown) {
+            EA.showErrorDialog("Backend error: " + xhr.status, errorThrown);
+        },
+        complete:function () {
+            $.mobile.loading("hide");
+        }
+    });
+})

a8800e7fd197ec9e82f85161063b05f2f0d4f1e1
Tim Ameye <Tim@10-92-99-236.kotnet.org>
2012-11-20 11:53:42 +0100

    preparing overview for sorting by date

--- jQuery Mobile/js/ea-overview.js
+++ jQuery Mobile/js/ea-overview.js
@@ -14,20 +14,29 @@ $(document).on("pageshow", "#overview", function () {
             $.mobile.loading("show");
         },
         success:function (xml) {
-            $(xml).find("expenseForm").each(function () {
+            var expenseForms = new Array();
+
+            $(xml).find("expenseForm").each(function (i) {
+                // making an object from it
+                var expenseForm = new Object();
+
                 // put $(this) in avariable for performance reasons
                 var $this = $(this);
 
                 // > finds the direct descendant in tree
-                var date = new Date($this.find(">date").text());
-                var id = $this.find(">id").text();
+                expenseForm.date = new Date($this.find(">date").text());
+                expenseForm.id = $this.find(">id").text();
 
-                var dateString = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
-                $("#overview-list").append("<li><a id=\"overview-show-pdf-" + id + "\"><h1>" + dateString + "</h1><p>Status</p></a></li>");
+                expenseForms.push(expenseForm);
             });
 
-            if ($("#overview-list li").size() == 0) {
+            if(expenseForms.length == 0){
                 $("#overview-list").append("<li>No expenses submitted.</li>");
+            }else{
+                $.each(expenseForms, function(i,expense) {
+                    var dateString = expense.date.getDate() + "/" + expense.date.getMonth() + "/" + expense.date.getFullYear();
+                    $("#overview-list").append("<li><a id=\"overview-show-pdf-" + expense.id + "\"><h1>" + dateString + "</h1><p>Status</p></a></li>");
+                });
             }
 
             $("#overview-list").listview("refresh");

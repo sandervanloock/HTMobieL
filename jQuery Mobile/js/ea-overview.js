8ae86ef55ca34d6a7f46445eaa8aaa4129e30c33
Tim Ameye <Tim@10-92-99-236.kotnet.org>
2012-11-20 14:00:17 +0100

    buttons added in step 2

--- jQuery Mobile/js/ea-overview.js
+++ jQuery Mobile/js/ea-overview.js
@@ -17,10 +17,10 @@ $(document).on("pageshow", "#overview", function () {
             var expenseForms = new Array();
 
             $(xml).find("expenseForm").each(function (i) {
-                // making an object from it
+                // making an object for it
                 var expenseForm = new Object();
 
-                // put $(this) in avariable for performance reasons
+                // put $(this) in a variable for performance reasons
                 var $this = $(this);
 
                 // > finds the direct descendant in tree
@@ -30,10 +30,10 @@ $(document).on("pageshow", "#overview", function () {
                 expenseForms.push(expenseForm);
             });
 
-            if(expenseForms.length == 0){
+            if (expenseForms.length == 0) {
                 $("#overview-list").append("<li>No expenses submitted.</li>");
-            }else{
-                $.each(expenseForms, function(i,expense) {
+            } else {
+                $.each(expenseForms, function (i, expense) {
                     var dateString = expense.date.getDate() + "/" + expense.date.getMonth() + "/" + expense.date.getFullYear();
                     $("#overview-list").append("<li><a id=\"overview-show-pdf-" + expense.id + "\"><h1>" + dateString + "</h1><p>Status</p></a></li>");
                 });
@@ -54,7 +54,6 @@ $(document).on("tap", "[id^=overview-show-pdf]", function () {
     var expenseFormId = $(this).attr("id").replace("overview-show-pdf-", "");
     $.ajax({
         type:"POST",
-        //dataType:"xml",
         url:"http://kulcapexpenseapp.appspot.com/resources/expenseService/getExpenseFormPDF",
         data:{
             'token':EA.token,
@@ -63,8 +62,8 @@ $(document).on("tap", "[id^=overview-show-pdf]", function () {
         beforeSend:function () {
             $.mobile.loading("show");
         },
-        success:function (pdf) {
-            console.log(pdf);
+        success:function (data) {
+            pdf = data;
         },
         error:function (xhr, textStatus, errorThrown) {
             EA.showErrorDialog("Backend error: " + xhr.status, errorThrown);

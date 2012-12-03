6f4f5e04fdfbf72561b35053809cf8ec11d210c0
Tim Ameye <Tim@10-92-99-236.kotnet.org>
2012-12-03 12:58:44 +0100

    fetch project codes at logon time

--- jQuery Mobile/js/ea/functions.js
+++ jQuery Mobile/js/ea/functions.js
@@ -9,42 +9,7 @@ var EA = {
         "G20AZERRR"
     ],
 
-    currencies:[
-        "EUR",
-        "USD",
-        "JPY",
-        "BGN",
-        "CZK",
-        "DKK",
-        "GBP",
-        "HUF",
-        "LTL",
-        "LVL",
-        "PLN",
-        "RON",
-        "SEK",
-        "CHF",
-        "NOK",
-        "HRK",
-        "RUB",
-        "TRY",
-        "AUD",
-        "BRL",
-        "CAD",
-        "CNY",
-        "HKD",
-        "IDR",
-        "ILS",
-        "INR",
-        "KRW",
-        "MXN",
-        "MYR",
-        "NZD",
-        "PHP",
-        "SGD",
-        "THB",
-        "ZAR"
-    ],
+    currencies:new Array(),
 
     isLoggedIn:function () {
         if (this.token == null) {
@@ -59,14 +24,18 @@ var EA = {
         $.mobile.changePage("#login");
     },
 
-    showErrorDialog:function (title, message) {
+    showError:function (title, message) {
         $("#errortitle").text(title);
         $("#errormessage").text(message);
         $.mobile.changePage("#error");
     },
 
-    showBackendErrorDialog:function (message) {
-        this.showErrorDialog("Backend error", message);
+    showBackendError:function (message) {
+        this.showError("Backend error", message);
+    },
+
+    showValidationError:function (message) {
+        this.showError("Validation error", message);
     }
 
 };
--- jQuery Mobile/js/ea/login.js
+++ jQuery Mobile/js/ea/login.js
@@ -16,13 +16,14 @@ $(document).on("pageinit", "#login", function () {
                 },
                 success:function (data) {
                     if (data == '') {
-                        EA.showErrorDialog("Login error", "The username and/or password are incorrect.");
+                        EA.showError("Login error", "The username and/or password are incorrect.");
                     } else {
                         EA.token = data;
                         console.log("User was logged in successfully: " + EA.token);
+                        $.mobile.changePage("#home");
 
-                        // fetch projectcodes at logon time
-                        $.ajax({
+                        // fetch projectcodes asynchronous at logon time
+                        /*$.ajax({
                             type:"POST",
                             dataType:"json",
                             url:"http://kulcapexpenseapp.appspot.com/resources/expenseService/getProjectCodeSuggestion",
@@ -31,10 +32,34 @@ $(document).on("pageinit", "#login", function () {
                             },
                             success:function (data) {
                                 console.log(data);
-                                $.mobile.changePage("#home");
                             },
                             error:function (xhr, textStatus, errorThrown) {
-                                EA.showBackendErrorDialog("Could not fetch project codes.");
+                                EA.showBackendError("Could not fetch project codes.");
+                            },
+                            complete:function () {
+                                $.mobile.loading("hide");
+                            }
+                        });*/
+
+                        // fetch currencies asynchronous at logon time
+                        $.ajax({
+                            type:"POST",
+                            dataType:"xml",
+                            url:"http://kulcapexpenseapp.appspot.com/resources/currencyService/getCurrencies",
+                            beforeSend:function () {
+                                $.mobile.loading("show");
+                            },
+                            success:function (xml) {
+                                var $xml = $("Cube", xml);
+                                $xml.find("Cube").each(function () {
+                                    var $this = $(this);
+                                    var currency = $this.attr("currency");
+                                    var rate = $this.attr("rate");
+                                    EA.currencies.push(currency);
+                                });
+                            },
+                            error:function (xhr, textStatus, errorThrown) {
+                                EA.showBackendError("Could not fetch currencies.");
                             },
                             complete:function () {
                                 $.mobile.loading("hide");
@@ -43,7 +68,7 @@ $(document).on("pageinit", "#login", function () {
                     }
                 },
                 error:function (xhr, textStatus, errorThrown) {
-                    EA.showBackendErrorDialog("Could not log in.");
+                    EA.showBackendError("Could not log in.");
                 },
                 complete:function () {
                     $.mobile.loading("hide");
@@ -51,7 +76,7 @@ $(document).on("pageinit", "#login", function () {
             });
         },
         invalidHandler:function (form, validator) {
-            EA.showErrorDialog("Validation error", "Some of the fields were not filled in correctly. Please correct the indicated fields.");
+            EA.showValidationError("Some of the fields were not filled in correctly. Please correct the indicated fields.");
         }
     });
 });

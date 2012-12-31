2dc35f75712ea3734373089f8ecb7112db9459ae
Tim Ameye <Tim@10-92-97-165.kotnet.org>
2012-12-31 09:14:45 +0100

    id for expense and overview

--- jQuery Mobile/js/ea/overview.js
+++ jQuery Mobile/js/ea/overview.js
@@ -7,7 +7,23 @@ $(document).on("pagebeforeshow", "#overview", function () {
         $.each(EA.localExpenses, function (index, expense) {
             var li = "<li><a href=\"#\">";
             li += "<h1>" + expense.date + " </h1>";
-            li += "<p>" + expense.remarks + " (" + expense.amount + " " + expense.currency + ")</p>"
+
+            var description;
+            if (expense.expenseTypeId == 1) {
+                description = "Hotel";
+            } else if (expense.expenseTypeId == 2) {
+                description = "Lunch";
+            } else if (expense.expenseTypeId == 3) {
+                description = "Diner";
+            } else if (expense.expenseTypeId == 4) {
+                description = "Ticket";
+            } else if (expense.expenseTypeId == 5) {
+                description = "Restaurant";
+            } else {
+                description = "Other";
+            }
+
+            li += "<p>" + description + " (" + expense.amount + " " + expense.currency + ")</p>"
             li += "</a></li>"
             $("#overview-list").append(li);
         });

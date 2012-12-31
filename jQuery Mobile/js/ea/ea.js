d2b6de8c434d6787d6fd736471bd35d17f722133
Tim Ameye <Tim@10-92-97-165.kotnet.org>
2012-12-31 09:47:51 +0100

    sorting expenses in overview

--- jQuery Mobile/js/ea/ea.js
+++ jQuery Mobile/js/ea/ea.js
@@ -45,6 +45,47 @@ var EA = {
         $("#error-validation-items").html(html);
 
         validator.defaultShowErrors();
+    },
+
+    sortExpensesAscending:function (a, b) {
+        var dateA = new Date(a.date);
+        var dateB = new Date(b.date);
+        return (dateA - dateB);
+    },
+
+    toBelgianDate:function (date) {
+        // get properties
+        var dd = date.getDate();
+        var mm = date.getMonth();
+
+        // January is 0, so plus 1
+        mm += 1;
+
+        // Leading zeros
+        if (dd < 10) {
+            dd = "0" + dd;
+        }
+        if (mm < 10) {
+            mm = "0" + mm;
+        }
+
+        return dd + "/" + mm + "/" + date.getFullYear();
+    },
+
+    expenseTypeIdToString:function (id) {
+        if (id == 1) {
+            return "Hotel";
+        } else if (id == 2) {
+            return "Lunch";
+        } else if (id == 3) {
+            return "Diner";
+        } else if (id == 4) {
+            return "Ticket";
+        } else if (id == 5) {
+            return "Restaurant";
+        } else {
+            return "Other";
+        }
     }
 
 };
--- jQuery Mobile/js/ea/overview.js
+++ jQuery Mobile/js/ea/overview.js
@@ -4,26 +4,14 @@ $(document).on("pagebeforeshow", "#overview", function () {
     if (EA.localExpenses.length == 0) {
         $("#overview-list").append("<li>No local expenses submitted.</li>");
     } else {
-        $.each(EA.localExpenses, function (index, expense) {
+        // sort by ascending order
+        var sorted = EA.localExpenses.sort(EA.sortExpensesAscending);
+        // show them
+        $.each(sorted, function (index, expense) {
             var li = "<li><a href=\"#\">";
-            li += "<h1>" + expense.date + " </h1>";
-
-            var description;
-            if (expense.expenseTypeId == 1) {
-                description = "Hotel";
-            } else if (expense.expenseTypeId == 2) {
-                description = "Lunch";
-            } else if (expense.expenseTypeId == 3) {
-                description = "Diner";
-            } else if (expense.expenseTypeId == 4) {
-                description = "Ticket";
-            } else if (expense.expenseTypeId == 5) {
-                description = "Restaurant";
-            } else {
-                description = "Other";
-            }
-
-            li += "<p>" + description + " (" + expense.amount + " " + expense.currency + ")</p>"
+            li += "<h1>" + EA.toBelgianDate(new Date(expense.date)) + " </h1>";
+            li += "<p>" + EA.expenseTypeIdToString(expense.expenseTypeId);
+            li += " (" + expense.amount + " " + expense.currency + ")</p>"
             li += "</a></li>"
             $("#overview-list").append(li);
         });

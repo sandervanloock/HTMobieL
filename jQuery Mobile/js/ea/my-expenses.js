35e36a6ca5ec605fbd6f2cde4251d78b2f7fe0e4
Tim Ameye <Tim@sfw2-l.kulnet.kuleuven.be>
2013-01-09 19:59:42 +0100

    refactor my expenses

--- jQuery Mobile/js/ea/my-expenses.js
+++ jQuery Mobile/js/ea/my-expenses.js
@@ -1,4 +1,5 @@
 $(document).on("pagebeforeshow", "#my-expenses", function () {
+    // load the expense forms that were cached from the server
     var serverExpenses = EA.getServerExpenses();
     var $expenseList = $("#my-expenses-list");
     var li;
@@ -15,7 +16,7 @@ $(document).on("pagebeforeshow", "#my-expenses", function () {
 });
 
 $(document).on("pageshow", "#my-expenses", function () {
-    // synchronize local expense forms with those on the server
+    // when the page is shown, check if the server has new expense forms
     $.ajax({
         type:"POST",
         dataType:"xml",
@@ -26,26 +27,29 @@ $(document).on("pageshow", "#my-expenses", function () {
         beforeSend:function () {
             $.mobile.loading("show");
         },
+        complete:function () {
+            $.mobile.loading("hide");
+        },
         success:function (xml) {
-            // empty local expenses
+            // empty cache of server expense forms
             EA.emptyServerExpenses();
 
-            // parse the XML
+            // parse the XML response
             $(xml).find("expenseForm").each(function () {
-                var expenseForm = {};
-
                 // put $(this) in a variable for performance reasons
                 var $this = $(this);
 
+                var expenseForm = {};
                 // > finds the direct descendant in tree
                 expenseForm.date = new Date($this.find(">date").text());
                 expenseForm.id = $this.find(">id").text();
                 expenseForm.statusId = $this.find(">statusId").text();
 
+                // add the expense form
                 EA.addServerExpense(expenseForm.id, expenseForm);
             });
 
-            // sort the expenses in descending order
+            // sort the expense forms in descending order
             var expenseForms = EA.getServerExpenses().sort(EA.sortExpensesDescending);
 
             // hold list in local variable for performance
@@ -54,7 +58,7 @@ $(document).on("pageshow", "#my-expenses", function () {
             // empty list
             $expenseList.empty();
 
-            // show expenses
+            // set expense forms
             if (expenseForms.length == 0) {
                 $expenseList.append("<li>No expenses submitted.</li>");
             } else {
@@ -68,26 +72,24 @@ $(document).on("pageshow", "#my-expenses", function () {
                 });
             }
 
-            // refresh the list
+            // refresh the list so it will be shown with proper jQM layout
             $expenseList.listview("refresh");
         },
-        error:function (xhr, textStatus, errorThrown) {
+        error:function () {
             EA.showBackendError("Could not fetch expenses from server.");
-        },
-        complete:function () {
-            $.mobile.loading("hide");
         }
     });
 });
 
+// when the users taps on an expense form, download it in PDF-format
 $(document).on("tap", "[id^=my-expenses-show-pdf]", function () {
-    // get the id that is requested
+    // get the id of the form that is requested
     var expenseFormId = $(this).attr("id").replace("my-expenses-show-pdf-", "");
-    // guideline: AJAX is not for fetching raw data like a PDF
-    // To accomplish this, a hiden form is used and the requested data is
+    // guideline: AJAX is not for fetching raw data like a PDF.
+    // to accomplish this, a hidden form is used and the requested data is
     // copied into that hidden form
     $("#my-expenses-token").val(EA.getToken());
     $("#my-expenses-form-id").val(expenseFormId);
-    // submit that hidden form
+    // submit that hidden form so the PDF will be downloaded
     $('#my-expenses-form').submit();
 });

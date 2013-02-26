853f8badad32b3c91034919f614d17033ae41a36
Tim Ameye <Tim@sfw2-l.kulnet.kuleuven.be>
2013-02-26 11:44:10 +0100

    testing in lan

--- jQuery Mobile/js/ea/ea.js
+++ jQuery Mobile/js/ea/ea.js
@@ -2,6 +2,7 @@ var EA = {
 
     baseURL:"http://kulcapexpenseapp.appspot.com/",
 //    baseURL:"http://localhost:8888/",
+//    baseURL:"http://192.168.1.11:8888/",
 
     /*************************************************
      * Acces token

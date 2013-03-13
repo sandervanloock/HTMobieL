ce409835acd4ce181330b3deb35098c32a97180a
sandervanloock <sander.vanloock1@student.kuleuven.be>
2013-03-13 03:53:02 +0100

    svn folded deleted

---
+++ KendoUI/wrappers/aspnetmvc/Examples/Controllers/Web/Editor/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class EditorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ImageBrowser()
+        {
+            return View();
+        }
+    }
+}
---
+++ KendoUI/wrappers/aspnetmvc/Examples/Models/StockDataPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kendo.Mvc.Examples.Models
+{
+    public class StockDataPoint
+    {
+        public DateTime Date { get; set; }
+
+        public decimal Close { get; set; }
+
+        public long Volume { get; set; }
+
+        public decimal Open { get; set; }
+
+        public decimal High { get; set; }
+
+        public decimal Low { get; set; }
+
+        public string Symbol { get; set; }
+    }
+}

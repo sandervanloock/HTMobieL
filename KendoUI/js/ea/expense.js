e985e481f65dd03722dee2bbe067a47aa22cff0b
sandervanloock <sander.vanloock1@student.kuleuven.be>
2013-03-23 18:41:29 +0100

    projectcode en amount zijn gebind

--- KendoUI/js/ea/expense.js
+++ KendoUI/js/ea/expense.js
@@ -1,82 +1,37 @@
-var Expense = kendo.data.Model.define( {
-    id: "expenseId",
-    expenses: [],
-    fields :
-    {
-        "expenseTypeId":{
-            type : 'number'
-        },
-        "expenseLocationId": {
-            type : 'number'
-        },
-        "date" :{
-            type : 'date'
-        },
-        "projectCode": {
-            type : 'number'
-        },
-        "currency": {
-            type : 'string'
-        },
-        "amount": {
-            type : 'number'
-        },
-        "remarks": {
-            type : 'string'
-        },
-        "evidence" : {
-            type: 'string'
-        }
-    },
-    create: function(e){
-        // add the items to the array of expenses
-        console.log(this);
-        this.get("expenses").push({
-            //expenseTypeId: this.get("expenseType"),
-            expenseLocationId: this.get("expenseLocationId"),
-            date: this.get("date"),
-            projectCode: this.get("projectCode"),
-            currency: this.get("currency"),
-            amount: this.get("amount"),
-            remarks: this.get("remarks")
-        });
-        console.log(this.get("expenses"));
-        // reset the form TODO
-        /*this.set("expenseType", "food");
-        this.set("merchant", "");
-        this.set("amount", "");*/
-    }
-
-});
-
 var viewModel = kendo.observable({
 
     // expenses array will hold the grid values
     expenses: [],
 
-    // type array populates the drop down
-    //type: [{ name: "Food", value: "food"}, { name: "Clothing", value: "clothing"}, { name: "Bills", value: "bills" }],
-
-    // expenseType holds the currently selected value of the dropdown list
-    //expenseType: "food",
-
     // the values are bound to the merchant and amount fields
-    merchant: null,
+    //date: null,
+    projectCode: null,
+    //expenseTypeId: null,
     amount: null,
+    //currency: null,
+    remarks: null,
+    //expenseLocationId: 1,
 
     // event execute on click of add button
     create: function(e) {
 
         // add the items to the array of expenses
         this.get("expenses").push({
-            //Type: this.get("expenseType"),
-            Merchant: this.get("projectCode"),
-            Amount: this.get("amount")});
-
+            //date: this.get("date"),
+            projectCode: this.get("projectCode"),
+            //expenseTypeId: this.get("expenseTypeId"),
+            amount: this.get("amount"),
+            //currency: this.get("currency"),
+            remarks: this.get("remarks")
+        }),
         // reset the form
         //this.set("expenseType", "food");
+        //this.set("date", "");
         this.set("projectCode", "");
+        //this.set("expenseTypeId", "");
         this.set("amount", "");
+        //this.set("currency", "");
+        //this.set("remarks", "");
         console.log( this.get("expenses"));
     }
 

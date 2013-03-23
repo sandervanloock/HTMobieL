5dd6746642c35ea0cbe5d62526bed43e0bbf8c2a
sandervanloock <sander.vanloock1@student.kuleuven.be>
2013-03-23 19:02:47 +0100

    projectcode en amount zijn gebind

--- KendoUI/js/ea/expense.js
+++ KendoUI/js/ea/expense.js
@@ -1,37 +1,31 @@
+//example at http://www.kendoui.com/blogs/teamblog/posts/12-03-09/bind_this_a_look_at_kendo_ui_mvvm.aspx
 var viewModel = kendo.observable({
 
     // expenses array will hold the grid values
     expenses: [],
 
     // the values are bound to the merchant and amount fields
-    //date: null,
+    date: new Date(),
     projectCode: null,
-    //expenseTypeId: null,
+    expenseTypeId: null,
     amount: null,
     //currency: null,
     remarks: null,
-    //expenseLocationId: 1,
+    expenseLocationId: 1,
 
     // event execute on click of add button
     create: function(e) {
-
+        console.log(e);
         // add the items to the array of expenses
         this.get("expenses").push({
-            //date: this.get("date"),
+            date: kendo.toString(this.get("date"),"dd/MM/yyyy"),
             projectCode: this.get("projectCode"),
-            //expenseTypeId: this.get("expenseTypeId"),
+            expenseTypeId: this.get("expenseTypeId"),
             amount: this.get("amount"),
             //currency: this.get("currency"),
             remarks: this.get("remarks")
         }),
-        // reset the form
-        //this.set("expenseType", "food");
-        //this.set("date", "");
-        this.set("projectCode", "");
-        //this.set("expenseTypeId", "");
-        this.set("amount", "");
-        //this.set("currency", "");
-        //this.set("remarks", "");
+        // TODO reset the form
         console.log( this.get("expenses"));
     }
 

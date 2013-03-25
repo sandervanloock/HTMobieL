29f3334fc0ff68405b0ffce7145760767fb8cc15
sandervanloock <sander.vanloock1@student.kuleuven.be>
2013-03-25 20:50:35 +0100

    uncommited files

--- Sencha/build/ExpenseApp/production/resources/js/compatibility.js
+++ Sencha/build/ExpenseApp/production/resources/js/compatibility.js
@@ -1,1 +1,399 @@
-/*bd31c7d5bf4b0c72936afe84ab96d8c2935b3f04*/"use strict";(function checkTypedArrayCompatibility(){if(typeof Uint8Array!=="undefined"){if(typeof Uint8Array.prototype.subarray==="undefined"){Uint8Array.prototype.subarray=function b(e,d){return new Uint8Array(this.slice(e,d))};Float32Array.prototype.subarray=function b(e,d){return new Float32Array(this.slice(e,d))}}if(typeof Float64Array==="undefined"){window.Float64Array=Float32Array}return}function b(e,d){return new a(this.slice(e,d))}function c(g,e){if(arguments.length<2){e=0}for(var d=0,f=g.length;d<f;++d,++e){this[e]=g[d]&255}}function a(e){var d;if(typeof e==="number"){d=[];for(var f=0;f<e;++f){d[f]=0}}else{d=e.slice(0)}d.subarray=b;d.buffer=d;d.byteLength=d.length;d.set=c;if(typeof e==="object"&&e.buffer){d.buffer=e.buffer}return d}window.Uint8Array=a;window.Uint32Array=a;window.Int32Array=a;window.Uint16Array=a;window.Float32Array=a;window.Float64Array=a})();(function checkObjectCreateCompatibility(){if(typeof Object.create!=="undefined"){return}Object.create=function a(c){var b=function d(){};b.prototype=c;return new b()}})();(function checkObjectDefinePropertyCompatibility(){if(typeof Object.defineProperty!=="undefined"){var a=true;try{Object.defineProperty(new Image(),"id",{value:"test"})}catch(c){a=false}if(a){return}}Object.defineProperty=function b(g,e,f){delete g[e];if("get" in f){g.__defineGetter__(e,f.get)}if("set" in f){g.__defineSetter__(e,f.set)}if("value" in f){g.__defineSetter__(e,function d(i){this.__defineGetter__(e,function h(){return i});return i});g[e]=f.value}}})();(function checkObjectKeysCompatibility(){if(typeof Object.keys!=="undefined"){return}Object.keys=function a(d){var b=[];for(var c in d){if(d.hasOwnProperty(c)){b.push(c)}}return b}})();(function checkFileReaderReadAsArrayBuffer(){if(typeof FileReader==="undefined"){return}var b=FileReader.prototype;if("readAsArrayBuffer" in b){return}Object.defineProperty(b,"readAsArrayBuffer",{value:function a(d){var c=new FileReader();var e=this;c.onload=function f(h){var n=h.target.result;var g=new ArrayBuffer(n.length);var m=new Uint8Array(g);for(var j=0,k=n.length;j<k;j++){m[j]=n.charCodeAt(j)}Object.defineProperty(e,"result",{value:g,enumerable:true,writable:false,configurable:true});var l=document.createEvent("HTMLEvents");l.initEvent("load",false,false);e.dispatchEvent(l)};c.readAsBinaryString(d)}})})();(function checkXMLHttpRequestResponseCompatibility(){var b=XMLHttpRequest.prototype;if(!("overrideMimeType" in b)){Object.defineProperty(b,"overrideMimeType",{value:function c(f){}})}if("response" in b||"mozResponseArrayBuffer" in b||"mozResponse" in b||"responseArrayBuffer" in b){return}if(typeof VBArray!=="undefined"){Object.defineProperty(b,"response",{get:function e(){return new Uint8Array(new VBArray(this.responseBody).toArray())}});return}function d(){this.overrideMimeType("text/plain; charset=x-user-defined")}if(typeof b.overrideMimeType==="function"){Object.defineProperty(b,"responseType",{set:d})}function a(){var h=this.responseText;var g,j=h.length;var f=new Uint8Array(j);for(g=0;g<j;++g){f[g]=h.charCodeAt(g)&255}return f}Object.defineProperty(b,"response",{get:a})})();(function checkWindowBtoaCompatibility(){if("btoa" in window){return}var b="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";window.btoa=function a(h){var f="";var g,e;for(g=0,e=h.length;g<e;g+=3){var l=h.charCodeAt(g)&255;var k=h.charCodeAt(g+1)&255;var j=h.charCodeAt(g+2)&255;var d=l>>2,c=((l&3)<<4)|(k>>4);var o=g+1<e?((k&15)<<2)|(j>>6):64;var m=g+2<e?(j&63):64;f+=(b.charAt(d)+b.charAt(c)+b.charAt(o)+b.charAt(m))}return f}})();(function checkFunctionPrototypeBindCompatibility(){if(typeof Function.prototype.bind!=="undefined"){return}Function.prototype.bind=function a(e){var d=this,b=Array.prototype.slice.call(arguments,1);var c=function f(){var g=Array.prototype.concat.apply(b,arguments);return d.apply(e,g)};return c}})();(function checkDataURICompatibility(){if(!("documentMode" in document)||document.documentMode!==9&&document.documentMode!==10){return}var b=Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype,"src");Object.defineProperty(HTMLIFrameElement.prototype,"src",{get:function c(){return this.$src},set:function a(e){this.$src=e;if(e.substr(0,14)!="data:text/html"){b.set.call(this,e);return}b.set.call(this,"about:blank");setTimeout((function d(){var f=this.contentDocument;f.open("text/html");f.write(e.substr(e.indexOf(",")+1));f.close()}).bind(this),0)},enumerable:true})})();(function checkDatasetProperty(){var a=document.createElement("div");if("dataset" in a){return}Object.defineProperty(HTMLElement.prototype,"dataset",{get:function(){if(this._dataset){return this._dataset}var f={};for(var b=0,e=this.attributes.length;b<e;b++){var d=this.attributes[b];if(d.name.substring(0,5)!="data-"){continue}var c=d.name.substring(5).replace(/\-([a-z])/g,function(h,g){return g.toUpperCase()});f[c]=d.value}Object.defineProperty(this,"_dataset",{value:f,writable:false,enumerable:false});return f},enumerable:true})})();(function checkClassListProperty(){var c=document.createElement("div");if("classList" in c){return}function a(f,i,j,d){var g=f.className||"";var h=g.split(/\s+/g);if(h[0]==""){h.shift()}var e=h.indexOf(i);if(e<0&&j){h.push(i)}if(e>=0&&d){h.splice(e,1)}f.className=h.join(" ")}var b={add:function(d){a(this.element,d,true,false)},remove:function(d){a(this.element,d,false,true)},toggle:function(d){a(this.element,d,true,true)}};Object.defineProperty(HTMLElement.prototype,"classList",{get:function(){if(this._classList){return this._classList}var d=Object.create(b,{element:{value:this,writable:false,enumerable:true}});Object.defineProperty(this,"_classList",{value:d,writable:false,enumerable:false});return d},enumerable:true})})();(function checkConsoleCompatibility(){if(typeof console=="undefined"){console={log:function(){}}}})();(function checkOnClickCompatibility(){function b(c){if(a(c.target)){c.stopPropagation()}}function a(c){return c.disabled||(c.parentNode&&a(c.parentNode))}if(navigator.userAgent.indexOf("Opera")!=-1){document.addEventListener("click",b,true)}})();(function checkNavigatorLanguage(){if("language" in navigator){return}Object.defineProperty(navigator,"language",{get:function a(){var b=navigator.userLanguage||"en-US";return b.substring(0,2).toLowerCase()+b.substring(2).toUpperCase()},enumerable:true})})();
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+// Checking if the typed arrays are supported
+(function checkTypedArrayCompatibility() {
+  if (typeof Uint8Array !== 'undefined') {
+    // some mobile versions do not support subarray (e.g. safari 5 / iOS)
+    if (typeof Uint8Array.prototype.subarray === 'undefined') {
+        Uint8Array.prototype.subarray = function subarray(start, end) {
+          return new Uint8Array(this.slice(start, end));
+        };
+        Float32Array.prototype.subarray = function subarray(start, end) {
+          return new Float32Array(this.slice(start, end));
+        };
+    }
+
+    // some mobile version might not support Float64Array
+    if (typeof Float64Array === 'undefined')
+      window.Float64Array = Float32Array;
+
+    return;
+  }
+
+  function subarray(start, end) {
+    return new TypedArray(this.slice(start, end));
+  }
+
+  function setArrayOffset(array, offset) {
+    if (arguments.length < 2)
+      offset = 0;
+    for (var i = 0, n = array.length; i < n; ++i, ++offset)
+      this[offset] = array[i] & 0xFF;
+  }
+
+  function TypedArray(arg1) {
+    var result;
+    if (typeof arg1 === 'number') {
+      result = [];
+      for (var i = 0; i < arg1; ++i)
+        result[i] = 0;
+    } else
+      result = arg1.slice(0);
+
+    result.subarray = subarray;
+    result.buffer = result;
+    result.byteLength = result.length;
+    result.set = setArrayOffset;
+
+    if (typeof arg1 === 'object' && arg1.buffer)
+      result.buffer = arg1.buffer;
+
+    return result;
+  }
+
+  window.Uint8Array = TypedArray;
+
+  // we don't need support for set, byteLength for 32-bit array
+  // so we can use the TypedArray as well
+  window.Uint32Array = TypedArray;
+  window.Int32Array = TypedArray;
+  window.Uint16Array = TypedArray;
+  window.Float32Array = TypedArray;
+  window.Float64Array = TypedArray;
+})();
+
+// Object.create() ?
+(function checkObjectCreateCompatibility() {
+  if (typeof Object.create !== 'undefined')
+    return;
+
+  Object.create = function objectCreate(proto) {
+    var constructor = function objectCreateConstructor() {};
+    constructor.prototype = proto;
+    return new constructor();
+  };
+})();
+
+// Object.defineProperty() ?
+(function checkObjectDefinePropertyCompatibility() {
+  if (typeof Object.defineProperty !== 'undefined') {
+    // some browsers (e.g. safari) cannot use defineProperty() on DOM objects
+    // and thus the native version is not sufficient
+    var definePropertyPossible = true;
+    try {
+      Object.defineProperty(new Image(), 'id', { value: 'test' });
+    } catch (e) {
+      definePropertyPossible = false;
+    }
+    if (definePropertyPossible) return;
+  }
+
+  Object.defineProperty = function objectDefineProperty(obj, name, def) {
+    delete obj[name];
+    if ('get' in def)
+      obj.__defineGetter__(name, def['get']);
+    if ('set' in def)
+      obj.__defineSetter__(name, def['set']);
+    if ('value' in def) {
+      obj.__defineSetter__(name, function objectDefinePropertySetter(value) {
+        this.__defineGetter__(name, function objectDefinePropertyGetter() {
+          return value;
+        });
+        return value;
+      });
+      obj[name] = def.value;
+    }
+  };
+})();
+
+// Object.keys() ?
+(function checkObjectKeysCompatibility() {
+  if (typeof Object.keys !== 'undefined')
+    return;
+
+  Object.keys = function objectKeys(obj) {
+    var result = [];
+    for (var i in obj) {
+      if (obj.hasOwnProperty(i))
+        result.push(i);
+    }
+    return result;
+  };
+})();
+
+// No readAsArrayBuffer ?
+(function checkFileReaderReadAsArrayBuffer() {
+  if (typeof FileReader === 'undefined')
+    return; // FileReader is not implemented
+  var frPrototype = FileReader.prototype;
+  // Older versions of Firefox might not have readAsArrayBuffer
+  if ('readAsArrayBuffer' in frPrototype)
+    return; // readAsArrayBuffer is implemented
+  Object.defineProperty(frPrototype, 'readAsArrayBuffer', {
+    value: function fileReaderReadAsArrayBuffer(blob) {
+      var fileReader = new FileReader();
+      var originalReader = this;
+      fileReader.onload = function fileReaderOnload(evt) {
+        var data = evt.target.result;
+        var buffer = new ArrayBuffer(data.length);
+        var uint8Array = new Uint8Array(buffer);
+
+        for (var i = 0, ii = data.length; i < ii; i++)
+          uint8Array[i] = data.charCodeAt(i);
+
+        Object.defineProperty(originalReader, 'result', {
+          value: buffer,
+          enumerable: true,
+          writable: false,
+          configurable: true
+        });
+
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent('load', false, false);
+        originalReader.dispatchEvent(event);
+      };
+      fileReader.readAsBinaryString(blob);
+    }
+  });
+})();
+
+// No XMLHttpRequest.response ?
+(function checkXMLHttpRequestResponseCompatibility() {
+  var xhrPrototype = XMLHttpRequest.prototype;
+  if (!('overrideMimeType' in xhrPrototype)) {
+    // IE10 might have response, but not overrideMimeType
+    Object.defineProperty(xhrPrototype, 'overrideMimeType', {
+      value: function xmlHttpRequestOverrideMimeType(mimeType) {}
+    });
+  }
+  if ('response' in xhrPrototype ||
+      'mozResponseArrayBuffer' in xhrPrototype ||
+      'mozResponse' in xhrPrototype ||
+      'responseArrayBuffer' in xhrPrototype)
+    return;
+  // IE9 ?
+  if (typeof VBArray !== 'undefined') {
+    Object.defineProperty(xhrPrototype, 'response', {
+      get: function xmlHttpRequestResponseGet() {
+        return new Uint8Array(new VBArray(this.responseBody).toArray());
+      }
+    });
+    return;
+  }
+
+  // other browsers
+  function responseTypeSetter() {
+    // will be only called to set "arraybuffer"
+    this.overrideMimeType('text/plain; charset=x-user-defined');
+  }
+  if (typeof xhrPrototype.overrideMimeType === 'function') {
+    Object.defineProperty(xhrPrototype, 'responseType',
+                          { set: responseTypeSetter });
+  }
+  function responseGetter() {
+    var text = this.responseText;
+    var i, n = text.length;
+    var result = new Uint8Array(n);
+    for (i = 0; i < n; ++i)
+      result[i] = text.charCodeAt(i) & 0xFF;
+    return result;
+  }
+  Object.defineProperty(xhrPrototype, 'response', { get: responseGetter });
+})();
+
+// window.btoa (base64 encode function) ?
+(function checkWindowBtoaCompatibility() {
+  if ('btoa' in window)
+    return;
+
+  var digits =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+  window.btoa = function windowBtoa(chars) {
+    var buffer = '';
+    var i, n;
+    for (i = 0, n = chars.length; i < n; i += 3) {
+      var b1 = chars.charCodeAt(i) & 0xFF;
+      var b2 = chars.charCodeAt(i + 1) & 0xFF;
+      var b3 = chars.charCodeAt(i + 2) & 0xFF;
+      var d1 = b1 >> 2, d2 = ((b1 & 3) << 4) | (b2 >> 4);
+      var d3 = i + 1 < n ? ((b2 & 0xF) << 2) | (b3 >> 6) : 64;
+      var d4 = i + 2 < n ? (b3 & 0x3F) : 64;
+      buffer += (digits.charAt(d1) + digits.charAt(d2) +
+                 digits.charAt(d3) + digits.charAt(d4));
+    }
+    return buffer;
+  };
+})();
+
+// Function.prototype.bind ?
+(function checkFunctionPrototypeBindCompatibility() {
+  if (typeof Function.prototype.bind !== 'undefined')
+    return;
+
+  Function.prototype.bind = function functionPrototypeBind(obj) {
+    var fn = this, headArgs = Array.prototype.slice.call(arguments, 1);
+    var bound = function functionPrototypeBindBound() {
+      var args = Array.prototype.concat.apply(headArgs, arguments);
+      return fn.apply(obj, args);
+    };
+    return bound;
+  };
+})();
+
+// IE9/10 text/html data URI
+(function checkDataURICompatibility() {
+  if (!('documentMode' in document) ||
+      document.documentMode !== 9 && document.documentMode !== 10)
+    return;
+  // overriding the src property
+  var originalSrcDescriptor = Object.getOwnPropertyDescriptor(
+    HTMLIFrameElement.prototype, 'src');
+  Object.defineProperty(HTMLIFrameElement.prototype, 'src', {
+    get: function htmlIFrameElementPrototypeSrcGet() { return this.$src; },
+    set: function htmlIFrameElementPrototypeSrcSet(src) {
+      this.$src = src;
+      if (src.substr(0, 14) != 'data:text/html') {
+        originalSrcDescriptor.set.call(this, src);
+        return;
+      }
+      // for text/html, using blank document and then
+      // document's open, write, and close operations
+      originalSrcDescriptor.set.call(this, 'about:blank');
+      setTimeout((function htmlIFrameElementPrototypeSrcOpenWriteClose() {
+        var doc = this.contentDocument;
+        doc.open('text/html');
+        doc.write(src.substr(src.indexOf(',') + 1));
+        doc.close();
+      }).bind(this), 0);
+    },
+    enumerable: true
+  });
+})();
+
+// HTMLElement dataset property
+(function checkDatasetProperty() {
+  var div = document.createElement('div');
+  if ('dataset' in div)
+    return; // dataset property exists
+
+  Object.defineProperty(HTMLElement.prototype, 'dataset', {
+    get: function() {
+      if (this._dataset)
+        return this._dataset;
+
+      var dataset = {};
+      for (var j = 0, jj = this.attributes.length; j < jj; j++) {
+        var attribute = this.attributes[j];
+        if (attribute.name.substring(0, 5) != 'data-')
+          continue;
+        var key = attribute.name.substring(5).replace(/\-([a-z])/g,
+          function(all, ch) { return ch.toUpperCase(); });
+        dataset[key] = attribute.value;
+      }
+
+      Object.defineProperty(this, '_dataset', {
+        value: dataset,
+        writable: false,
+        enumerable: false
+      });
+      return dataset;
+    },
+    enumerable: true
+  });
+})();
+
+// HTMLElement classList property
+(function checkClassListProperty() {
+  var div = document.createElement('div');
+  if ('classList' in div)
+    return; // classList property exists
+
+  function changeList(element, itemName, add, remove) {
+    var s = element.className || '';
+    var list = s.split(/\s+/g);
+    if (list[0] == '') list.shift();
+    var index = list.indexOf(itemName);
+    if (index < 0 && add)
+      list.push(itemName);
+    if (index >= 0 && remove)
+      list.splice(index, 1);
+    element.className = list.join(' ');
+  }
+
+  var classListPrototype = {
+    add: function(name) {
+      changeList(this.element, name, true, false);
+    },
+    remove: function(name) {
+      changeList(this.element, name, false, true);
+    },
+    toggle: function(name) {
+      changeList(this.element, name, true, true);
+    }
+  };
+
+  Object.defineProperty(HTMLElement.prototype, 'classList', {
+    get: function() {
+      if (this._classList)
+        return this._classList;
+
+      var classList = Object.create(classListPrototype, {
+        element: {
+          value: this,
+          writable: false,
+          enumerable: true
+        }
+      });
+      Object.defineProperty(this, '_classList', {
+        value: classList,
+        writable: false,
+        enumerable: false
+      });
+      return classList;
+    },
+    enumerable: true
+  });
+})();
+
+// Check console compatability
+(function checkConsoleCompatibility() {
+  if (typeof console == 'undefined') {
+    console = {log: function() {}};
+  }
+})();
+
+// Check onclick compatibility in Opera
+(function checkOnClickCompatibility() {
+  // workaround for reported Opera bug DSK-354448:
+  // onclick fires on disabled buttons with opaque content
+  function ignoreIfTargetDisabled(event) {
+    if (isDisabled(event.target)) {
+      event.stopPropagation();
+    }
+  }
+  function isDisabled(node) {
+    return node.disabled || (node.parentNode && isDisabled(node.parentNode));
+  }
+  if (navigator.userAgent.indexOf('Opera') != -1) {
+    // use browser detection since we cannot feature-check this bug
+    document.addEventListener('click', ignoreIfTargetDisabled, true);
+  }
+})();
+
+// Checks if navigator.language is supported
+(function checkNavigatorLanguage() {
+  if ('language' in navigator)
+    return;
+  Object.defineProperty(navigator, 'language', {
+    get: function navigatorLanguage() {
+      var language = navigator.userLanguage || 'en-US';
+      return language.substring(0, 2).toLowerCase() +
+        language.substring(2).toUpperCase();
+    },
+    enumerable: true
+  });
+})();

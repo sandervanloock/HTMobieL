e3bcd755b2c3021e791bfcb0bdf178affd9f9db2
sandervanloock <sander.vanloock1@student.kuleuven.be>
2013-03-28 02:41:37 +0100

    binding expense

--- KendoUI/js/ea/expense.js
+++ KendoUI/js/ea/expense.js
@@ -1,36 +1,39 @@
 //example at http://www.kendoui.com/blogs/teamblog/posts/12-03-09/bind_this_a_look_at_kendo_ui_mvvm.aspx
-var viewModel = kendo.observable({
+var expenseForm = kendo.observable({
 
     // expenses array will hold the grid values
     expenses: [],
+
+    // type array populates the drop down
+    //type: [{ name: "Food", value: "food"}, { name: "Clothing", value: "clothing"}, { name: "Bills", value: "bills" }],
+
+    // expenseType holds the currently selected value of the dropdown list
+    //expenseType: "food",
 
     // the values are bound to the merchant and amount fields
     date: new Date(),
     projectCode: null,
     expenseTypeId: null,
     amount: null,
-    //currency: null,
-    remarks: "",
+    currency: null,
+    remarks: null,
     expenseLocationId: 1,
 
     // event execute on click of add button
     create: function(e) {
-        console.log(e);
-        // add the items to the array of expenses
         this.get("expenses").push({
             date: kendo.toString(this.get("date"),"dd/MM/yyyy"),
             projectCode: this.get("projectCode"),
             expenseTypeId: this.get("expenseTypeId"),
             amount: this.get("amount"),
-            //currency: this.get("currency"),
-            remarks: this.get("remarks")
-        }),
-        // TODO reset the form
-        console.log( this.get("expenses"));
+            currency: this.get("currency").get("currency"),
+            remarks: this.get("remarks"),
+            expenseLocationId: this.get("expenseLocationId")
+        });
+        //TODO reset the form
+        kendo.bind($("#overview-list"),expenseForm);
     }
 
 });
 
-// apply the bindings
-kendo.bind($("#addExpense"), viewModel);
-kendo.bind($("#overview-list"), viewModel);
+kendo.bind($('#newExpense'), expenseForm);

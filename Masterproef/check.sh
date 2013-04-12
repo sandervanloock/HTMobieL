23e0ff71f24b0a3865c57032eb20889958058946
Tim Ameye <Tim@sfw2-l.kulnet.kuleuven.be>
2013-04-12 14:38:15 +0200

    rewritten vergelijking

--- Masterproef/check.sh
+++ Masterproef/check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #regexp="\b\(we\|men\|je\)\b"
-regexp="^[^%].*\b\(we\|men\|je\|framework\|device\)\b"
+regexp="^[^%].*\b\(we\|men\|je\|framework\|device\|plugin\)\b"
 #regexp="%.*\b\(we\|men\|je\)\b"
 
 sh search.sh $regexp

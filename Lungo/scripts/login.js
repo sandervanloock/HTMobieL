000987508e9225d430668fdea7ea79d955e2dacd
Tim Ameye <Tim@tim-mac.local>
2013-04-17 21:57:57 +0200

    error dialogs and loading screen

--- Lungo/scripts/login.js
+++ Lungo/scripts/login.js
@@ -1,16 +1,63 @@
 Lungo.dom("#login-screen-button").on("tap", function (event) {
-    console.log("making ajax request");
+    var email = $$('#login-screen-username').val();
+    var password = $$('#login-screen-password').val();
 
-    Lungo.Service.post(
-        EA.baseURL + 'resources/userService/login',
-        {
-            'email': "tim.ameye@student.kuleuven.be",
-            'password': "test123"
-        },
-        function (token) {
-            console.log(token);
-        },
-        'text'
-    );
+    // TODO: manual validation
+    if (email === "" || password === "") {
+        Lungo.Notification.error(
+            // Title
+            "Validation error",
+            // Description
+            "Please fill in the required fields.",
+            // Icon
+            "cancel",
+            // Time on screen
+            0,
+            // Callback function
+            null
+        );
+    } else {
+        console.log("making ajax request");
+
+        // show loading screen
+        Lungo.Notification.show();
+
+        // make the AJAX-request
+        Lungo.Service.post(
+            // url
+            EA.baseURL + 'resources/userService/login',
+            // data
+            {
+                'email': email,
+                'password': password
+            },
+            // callback
+            function (token) {
+                Lungo.Notification.hide();
+
+                if (token === '') {
+                    Lungo.Notification.error(
+                        // Title
+                        "Error",
+                        // Description
+                        "The username and/or password are incorrect.",
+                        // Icon
+                        "cancel",
+                        // Time on screen
+                        0,
+                        // Callback function
+                        null
+                    );
+                } else {
+                    console.log(token);
+                    // go to home section
+                    Lungo.Router.section("home");
+                }
+            }
+            ,
+            // type
+            'text'
+        );
+    }
 
 });

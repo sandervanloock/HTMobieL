e6db6ff7c38a95a9fbace073a2f6d151463908d0
sandervanloock <sander.vanloock1@student.kuleuven.be>
2013-04-17 04:25:22 +0200

    tabellen evaluatie

--- Referenties/generate.sh
+++ Referenties/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for f in "literatuurstudie" "vergelijking" "evaluatie"
+for f in "literatuurstudie" "vergelijking" "raamwerken" "evaluatie"
 do
     python bittexurl.py -i $f.bib -o $f-en.bib -e
     python bittexurl.py -i $f.bib -o $f-nl.bib -n

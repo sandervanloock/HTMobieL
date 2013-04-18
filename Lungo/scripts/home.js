6a747999e3a64c740cce98c0b506c31f6b7b9caa
Tim Ameye <Tim@tim-mac.local>
2013-04-18 23:19:55 +0200

    confirmation continuing form

--- Lungo/scripts/home.js
+++ Lungo/scripts/home.js
@@ -3,6 +3,32 @@ Lungo.dom("#home").on("load", function () {
     $$("#home-screen-name").text(user.firstName + " " + user.lastName);
 });
 
+Lungo.dom("#home-screen-add").on("tap", function () {
+    if (Lungo.Data.Storage.persistent("localExpenses")) {
+        Lungo.Notification.confirm({
+            icon: "check",
+            title: "Saved expense form",
+            description: "You already have a form filled in, would you like to continue that form?",
+            accept: {
+                label: "Yes",
+                callback: function () {
+                    Lungo.Router.section("create1");
+                }
+            },
+            cancel: {
+                label: "No",
+                callback: function () {
+                    // clear local expenses
+                    Lungo.Data.Storage.persistent("localExpenses", null);
+                    Lungo.Router.section("create1");
+                }
+            }
+        });
+    } else {
+        Lungo.Router.section("create1");
+    }
+});
+
 Lungo.dom("#home-logout").on("tap", function () {
     // show loading screen
     Lungo.Notification.show();

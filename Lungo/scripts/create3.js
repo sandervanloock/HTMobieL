c4d90dd395c409b1cc55fb2dff8cc2a971b60c49
Tim Ameye <Tim@tim-mac.local>
2013-04-19 08:53:38 +0200

    convert image with canvas

--- Lungo/scripts/create3.js
+++ Lungo/scripts/create3.js
@@ -16,6 +16,44 @@ Lungo.dom("#create3").on("load", function () {
     $$("#create3-add-type").find("option[value='2']").remove();
     $$("#create3-add-type").find("option[value='3']").remove();
     $$("#create3-add-type").find("option[value='4']").remove();
+
+    // evidence to base64 via FileReaderAPI and HTML5 canvas
+    $$('#create3-add-evidence')[0].onchange = function (e) {
+        // get the file
+        var file = e.target.files[0];
+
+        if (window.FileReader) {
+            // initialize reader
+            var reader = new FileReader();
+            // if the image was read, load it into the canvas
+            reader.onload = function (e) {
+                // get the canvas that is hidden on that page
+                var canvas = $$('#create3-add-canvas')[0];
+                var context = canvas.getContext('2d');
+                var img = new Image();
+                // if the image is in canvas, get base64
+                img.onload = function () {
+                    // set canvas dimensions to image dimensions
+                    canvas.width = this.width;
+                    canvas.height = this.height;
+                    // draw the image on the canvas
+                    context.drawImage(this, 0, 0);
+                    // get the base64 string
+                    var base64 = EA.base64WithoutPrefix(canvas.toDataURL());
+                    // TODO check if this can fail in Lungo
+                    Lungo.Data.Storage.session("tempEvidence", base64);
+                    // hide loading screen
+                    Lungo.Notification.hide();
+                };
+                img.src = e.target.result;
+            };
+            // show loading screen
+            Lungo.Notification.show();
+            reader.readAsDataURL(file);
+        } else {
+            alert("FileReaderAPI not supported");
+        }
+    };
 });
 
 Lungo.dom("#create3-add-button").on("tap", function () {
@@ -37,8 +75,7 @@ Lungo.dom("#create3-add-button").on("tap", function () {
         "remarks": $$("#create3-add-remarks").val(),
         "expenseTypeId": parseInt($$("#create3-add-type").val()),
         "expenseLocationId": expenseLocation,
-        // TODO below
-        "evidence": null
+        "evidence": Lungo.Data.Storage.session("tempEvidence")
     };
 
     var localExpenses = Lungo.Data.Storage.persistent("localExpenses");
@@ -48,6 +85,7 @@ Lungo.dom("#create3-add-button").on("tap", function () {
         localExpenses.push(expense);
         Lungo.Data.Storage.persistent("localExpenses", localExpenses);
     }
+    Lungo.Data.Storage.session("tempEvidence", null);
     Lungo.Router.section("create2");
 });
 

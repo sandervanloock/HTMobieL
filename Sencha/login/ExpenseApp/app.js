502f7d6a62aca27bea0efe2cabd8b6bac79abe68
Tim Ameye <Tim@sfw2-l.kulnet.kuleuven.be>
2013-05-03 20:41:04 +0200

    sencha touch login build

--- Sencha/login/ExpenseApp/app.js
+++ Sencha/login/ExpenseApp/app.js
@@ -1,3 +1,18 @@
+/*
+ This file is generated and updated by Sencha Cmd. You can edit this file as
+ needed for your application, but these edits will have to be merged by
+ Sencha Cmd when it performs code generation tasks such as generating new
+ models, controllers or views and when running "sencha app upgrade".
+
+ Ideally changes to this file would be limited and most work would be done
+ in other places (such as Controllers). If Sencha Cmd cannot merge your
+ changes and its generated code, it will produce a "merge conflict" that you
+ will need to resolve manually.
+ */
+
+// DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
+//@require @packageOverrides
+
 //<debug>
 Ext.Loader.setPath({
     'Ext': 'touch/src',
@@ -12,24 +27,23 @@ Ext.application({
         'Ext.MessageBox'
     ],
 
+    models:[
+        'Song',
+        'User'
+    ],
+
     views: [
-        'Login',
-        'List'
-    ],
-
-    controllers: [
+        'List',
         'Login'
-    ],
-
-    models: [
-        'User',
-        'Song'
     ],
 
     stores: [
         'Songs'
     ],
 
+    controllers: [
+        'Login'
+    ],
     icon: {
         '57': 'resources/icons/Icon.png',
         '72': 'resources/icons/Icon~ipad.png',

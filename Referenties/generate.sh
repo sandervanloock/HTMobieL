01bf155ea1ee2f983fc2928411f713572ca9fc34
Tim Ameye <Tim@sfw2-l.kulnet.kuleuven.be>
2013-05-12 12:02:03 +0200

    references only in one file

--- Referenties/generate.sh
+++ Referenties/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for f in "literatuurstudie" "vergelijking" "raamwerken" "evaluatie"
+for f in "alles"
 do
     python bittexurl.py -i $f.bib -o $f-en.bib -e
     python bittexurl.py -i $f.bib -o $f-nl.bib -n
